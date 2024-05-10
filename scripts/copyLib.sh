f099d7e4e22dead66b10d0ac117cf305c3158f40
Sweaterz <709218316@qq.com>
2024-05-10 14:52:40 +0800

    添加了三个需要的库opencv、jsoncpp、log4cpp

--- scripts/copyLib.sh
+++ scripts/copyLib.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-#cd ${0%\*}
+cd ${0%\*}\lib
+
 
 echo -e "\e[33m********* Start copying lib files to /opt *********\e[0m"
 
@@ -10,7 +11,7 @@ lib_files=("log4cpp" "jsoncpp" "opencv454")
 for lib_file in ${lib_files[*]}
 do
     echo -e "\e[33m********* Start to copy $lib_file *********\e[0m"
-    sudo cp -r /home/hk/workspace/install_files/software/$lib_file /opt/ && echo -e "\e[32mSuccessfully copied $lib_file\e[0m" || echo -e "\e[31mFailed to copy $lib_file\e[0m"
+    sudo cp -r ./$lib_file /opt/ && echo -e "\e[32mSuccessfully copied $lib_file\e[0m" || echo -e "\e[31mFailed to copy $lib_file\e[0m"
 done
 
 echo -e "\e[33m********* End of copying lib files to /opt *********\e[0m"
