cc55427cb1d62f0c502c6a06a04213fa33f61b3e
Sweaterz <709218316@qq.com>
2024-06-12 09:01:08 +0800

    增加了jetson_stats版本4.2.6，解决了4.2.8版本出现的问题

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,8 +5,7 @@
 echo "install pip3"
 sudo apt install python3-pip python3-dev
 python3 -m pip install --upgrade pip # 升级pip
-sudo -H pip3 install jetson-stats   # 安装Jetson-stats 管理工具（jtop)
+sudo -H pip3 install -U jetson-stats==4.2.6   # 安装Jetson-stats 管理工具（jtop)
 sudo apt install cmake              # 安装cmake
 sudo apt install libeigen3-dev      # 安装eigen3
 
-echo "安装完成，按任意建继续。Completed. Press any key to continue."
