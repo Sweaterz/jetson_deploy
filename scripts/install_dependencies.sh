5553874e53871bb7a85f8e37e6d78278a534e605
Sweaterz <709218316@qq.com>
2024-06-12 10:38:15 +0800

    修改下载bug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,11 +3,13 @@
 # 以及安装cmake、eigen等必要库
 
 echo "install pip3"
-sudo apt install python3-pip python3-dev
-python3 -m pip install --upgrade pip # 升级pip
-sudo apt install cmake              # 安装cmake
-sudo apt install libeigen3-dev      # 安装eigen3
-
+sudo apt install python3-pip python3-dev -y
+python3 -m pip install --upgrade pip    # 升级pip
+echo "install cmake"
+sudo apt install cmake -y               # 安装cmake
+echo "install eigen3"
+sudo apt install libeigen3-dev -y       # 安装eigen3
+echo "install jetson-stats"
 sudo -H pip3 install -U jetson-stats==4.2.6   # 安装Jetson-stats 管理工具（jtop)
 echo "操作完成，重新启动系统后生效。"
 read  -n 1 -p "需要立刻重启吗? (Y/N) " REPLY
